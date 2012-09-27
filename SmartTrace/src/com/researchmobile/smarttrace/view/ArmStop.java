88a5a8788fa0a65c5d32d537715f35e3ec2bdca0
Wuilder Alvarado <walvarado@researchmobile.co>
2012-09-26 23:45:44 -0600

    Finalizar Proceso
    Activada la opcion buscar para finalizar un proceso en Armar

--- SmartTrace/src/com/researchmobile/smarttrace/view/ArmStop.java
+++ SmartTrace/src/com/researchmobile/smarttrace/view/ArmStop.java
@@ -1,6 +1,9 @@
 package com.researchmobile.smarttrace.view;
 
+import com.researchmobile.smarttrace.entity.BarrelList;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,6 +19,7 @@ public class ArmStop extends Activity implements OnClickListener{
 	private Spinner origenSpinner;
 	private RadioButton allFinishRadioButton, parcialFinishRadioButton;
 	private Button finishButton;
+	private BarrelList list;
 	
 	public void onCreate(Bundle saveInstanceState){
 		super.onCreate(saveInstanceState);
@@ -26,8 +30,11 @@ public class ArmStop extends Activity implements OnClickListener{
 
 	
 	private void ComponentPrepare() {
+		setList(new BarrelList());
+		
 		setUserCodeEditText((EditText)findViewById(R.id.arm_stop_user_edittext));
-		setBarrilCodeEditText((EditText)findViewById(R.id.arm_start_barrel_edittext));
+		getUserCodeEditText().setOnClickListener(this);
+		setBarrilCodeEditText((EditText)findViewById(R.id.arm_stop_barril_edittext));
 		setInservibleTapaderaEditText((EditText)findViewById(R.id.arm_stop_inservible_tapadera_edittext));
 		setInservibleBarrilEditText((EditText)findViewById(R.id.arm_stop_inservible_barril_edittext));
 		
@@ -43,13 +50,47 @@ public class ArmStop extends Activity implements OnClickListener{
 
 
 	public void onClick(View view){
+		//Control Buttons
 		if (view == getFinishButton()){
 			
+		}
+		
+		//Control EditText
+		if (view == getUserCodeEditText()){
+			activeScanUser();
 		}
 		
 	}
 
 
+	private void activeScanUser() {
+		IntentIntegrator integrator = new IntentIntegrator(this);
+		integrator.initiateScan();
+		
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanResult != null) {
+			String barcode;
+			barcode = scanResult.getContents();
+			getUserCodeEditText().setText(barcode);
+			
+			int tamano = getList().getBarrelList().size();
+			int cont = 0;
+			for (int i = 0; i < tamano; i++){
+				if (getList().getBarrelList().get(i).getUser().equalsIgnoreCase(barcode)){
+					getBarrilCodeEditText().setText(getList().getBarrelList().get(i).getCode());
+					cont++;
+				}
+			}
+			if (cont==0){
+				getUserCodeEditText().setText("");
+				Toast.makeText(getBaseContext(), "No ha iniciado procesos", Toast.LENGTH_SHORT).show();
+			}
+			
+		}
+	}
 	public EditText getUserCodeEditText() {
 		return userCodeEditText;
 	}
@@ -137,6 +178,16 @@ public class ArmStop extends Activity implements OnClickListener{
 
 	public void setFinishButton(Button finishButton) {
 		this.finishButton = finishButton;
+	}
+
+
+	public BarrelList getList() {
+		return list;
+	}
+
+
+	public void setList(BarrelList list) {
+		this.list = list;
 	}
 	
 }
